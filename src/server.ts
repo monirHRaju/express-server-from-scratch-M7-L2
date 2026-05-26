4236f045735e5669f7f7baa5f016a2e8beadb430
monirHRaju <monirhraju@gmail.com>
2026-05-26 15:52:16 +0600

    express server created, neon postgreSQL connected, GET and POST created

---
+++ src/server.ts
@@ -0,0 +1,98 @@
+import express, { type Application, type Request, type Response } from "express"
+import {Pool} from "pg"
+
+const app: Application = express()
+const port = 5000
+const pool = new Pool({
+  connectionString : "postgresql://neondb_owner:npg_fAg5PqJWh9Ey@ep-wandering-dawn-ap6fxlqh-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require" 
+})
+
+app.use(express.json())
+app.use(express.text())
+app.use(express.urlencoded({extended: true}))
+
+const initDB = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(20),
+        email VARCHAR(30) UNIQUE NOT NULL,
+        password VARCHAR(20) NOT NULL,
+        is_active BOOLEAN DEFAULT true,
+        age INT,
+
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+      )
+      
+      `);
+      console.log("database connected successfully")
+
+  } catch (error) {
+    console.log(error)
+  }
+} 
+
+initDB()
+
+app.get('/', (req: Request, res: Response) => {
+  
+//   res.send('Express Server')
+  res.status(200).json({
+    "message" : "Server working"
+  })
+})
+
+app.get('/api/users', async (req: Request, res: Response)=> {
+  try {
+    const result = await pool.query(`
+        SELECT * FROM users
+      `)
+      res.status(400).json({
+        success : true,
+        message : "users retrieved successfully",
+        data    : result.rows 
+      })
+  } catch (error: any) {
+    res.status(500).json({
+        success : false,
+        message : "Failed retrieving users",
+        error   : error  
+      })
+  }
+})
+
+
+app.post('/api/users', async (req: Request, res: Response)=> {
+  const {name, email, password, age, is_active} = req.body;
+
+  try {
+      const result = await pool.query(
+        `
+        INSERT INTO users(name, email, password, age)
+        VALUES ($1, $2, $3, $4)
+        RETURNING *
+        `, [name, email, password, age]
+      )
+    // console.log(result)
+    
+    res.status(201).json({
+      message: "created",
+      rowCount: result.rowCount,
+      insertedId: result.rows[0].id,
+      data : result.rows[0]
+    })
+  } catch (error:any) {
+    res.status(500).json({
+      message: error.message,
+      error : error
+    })
+  }
+  
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
