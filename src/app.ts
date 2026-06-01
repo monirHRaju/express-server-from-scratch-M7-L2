85c5d26c8ab319b0ecd76e1de53f87aa6cdda1d8
monirHRaju <monirhraju@gmail.com>
2026-06-01 23:43:51 +0600

    app level middleware, logger created

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,40 @@
-import express, { response, type Application, type Request, type Response } from "express"
+import express, {
+  response,
+  type Application,
+  type Request,
+  type Response,
+} from "express";
 import { initDB, pool } from "./db";
 import { userRouter } from "./modules/user/user.route";
 import { profileRoute } from "./modules/profile/profile.route";
 import { authRouter } from "./modules/auth/auth.route";
+import fs from "fs";
 
+const app: Application = express();
 
-const app: Application = express()
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  const log = `\n Method->${req.method} , Time-> ${Date.now()}, URL-> ${req.url} \n`;
+  fs.appendFile("./logger/logger.txt", log, (err) => {
+    console.log(err);
+  });
 
+  next();
+});
 
-app.use(express.json())
-app.use(express.text())
-app.use(express.urlencoded({extended: true}))
+initDB();
 
-
-
-initDB()
-
-app.get('/', (req: Request, res: Response) => {
-  
-//   res.send('Express Server')
+app.get("/", (req: Request, res: Response) => {
+  //   res.send('Express Server')
   res.status(200).json({
-    "message" : "Server working"
-  })
-})
+    message: "Server working",
+  });
+});
 
-app.use('/api/users', userRouter)
-app.use('/api/profiles', profileRoute)
-app.use('/api/auth', authRouter)
+app.use("/api/users", userRouter);
+app.use("/api/profiles", profileRoute);
+app.use("/api/auth", authRouter);
 
-
-
-export default app
+export default app;
