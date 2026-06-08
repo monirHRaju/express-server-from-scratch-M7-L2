a1acba71ea366dccc7115e1e356333bdc67ce186
monirHRaju <monirhraju@gmail.com>
2026-06-08 23:14:23 +0600

    refactor: comment out unused getProfile function and clean up error handling in createProfile

--- src/modules/profile/profile.controller.ts
+++ src/modules/profile/profile.controller.ts
@@ -26,64 +26,60 @@ const getAllProfile = async (req: Request, res: Response) => {
     });
   }
 };
-const getProfile = async (req: Request, res: Response) => {
-  const { id } = req.params;
+// const getProfile = async (req: Request, res: Response) => {
+//   const { id } = req.params;
 
-  try {
-    const result = await profileService.getSingleProfileFromDB(id as string);
+//   try {
+//     const result = await profileService.getSingleProfileFromDB(id as string);
 
-    if (result.rowCount === 0) {
-      res.status(404).json({
-        status: false,
-        message: "profile not found!",
-        data: {},
-      });
-    }
+//     if (result.rowCount === 0) {
+//       res.status(404).json({
+//         status: false,
+//         message: "profile not found!",
+//         data: {},
+//       });
+//     }
 
-    res.status(200).json({
-      status: true,
-      message: "Profile retrieved success!",
-      data: result.rows[0],
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      status: false,
-      message: "Failed to find profile!",
-      error: error.message,
-    });
-  }
-};
+//     res.status(200).json({
+//       status: true,
+//       message: "Profile retrieved success!",
+//       data: result.rows[0],
+//     });
+//   } catch (error: any) {
+//     res.status(500).json({
+//       status: false,
+//       message: "Failed to find profile!",
+//       error: error.message,
+//     });
+//   }
+// };
 
 const createProfile = async (req: Request, res: Response) => {
   // const {user_id, bio, address, phone, gender} = req.body
   const result = await profileService.createProfileToDB(req.body);
 
-//   console.log(result)
+  //   console.log(result)
 
   try {
-
     if (result.rowCount === 0) {
-    res.status(500).json({
-      status: false,
-      message: "Failed! No User Found",
-      data: {},
-    });
-  }
+      res.status(500).json({
+        status: false,
+        message: "Failed! No User Found",
+        data: {},
+      });
+    }
 
     res.status(200).json({
       success: true,
       message: "Profile created successfully!",
       data: result.rows[0],
     });
-
-  } catch (error:any) {
-
+  } catch (error: any) {
     res.status(500).json({
       status: false,
       message: "Failed! No User Found",
       error: error.message,
     });
-
   }
 };
 
